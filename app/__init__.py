6f94713e2891662592dc1e7d67a7fdae5ab3bcdb
Harish <7906749+harish8@users.noreply.github.com>
2025-10-17 01:04:59 -0400

    Initial project setup with Flask and uv pyproject.toml

---
+++ app/__init__.py
@@ -0,0 +1,23 @@
+import os 
+
+from flask import Flask
+
+def create_app():
+    # Create and configure the app
+    app = Flask(__name__)
+    app.config.from_mapping(
+        SECRET_KEY='dev',
+        DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite')
+    )
+
+    # Ensure the instance folder exists
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+
+    @app.route('/hello')
+    def hello():
+        return 'Hello, World!'
+    
+    return app
